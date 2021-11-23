98ae71e196318c8ea955d1e7b08d9c6da175154a
Dozingfiretruck <1473454180@qq.com>
2021-11-23 22:15:28 +0800

    add:libwmarch.a支持编译

--- lib/xmake.lua
+++ lib/xmake.lua
@@ -81,20 +81,30 @@ target("wmcommon")
 
 target_end()
 
--- target("wmarch")
---     set_kind("static")
---     set_plat("cross")
---     set_arch("c-sky")
+target("wmarch")
+    set_kind("static")
+    set_plat("cross")
+    set_arch("c-sky")
     
---     add_files(sdkdir.."platform/arch/**.c")
---     add_files(sdkdir.."platform/arch/**.S")
---     add_includedirs(sdkdir.."include",{public = true})
---     add_includedirs(sdkdir.."include/driver",{public = true})
---     add_includedirs(sdkdir.."include/os",{public = true})
---     add_includedirs(sdkdir.."include/arch/xt804",{public = true})
---     add_includedirs(sdkdir.."include/arch/xt804/csi_core",{public = true})
+    add_files(sdkdir.."platform/arch/**.c")
+    add_files(sdkdir.."platform/arch/**.S")
+    add_includedirs(sdkdir.."include",{public = true})
+    add_includedirs(sdkdir.."include/driver",{public = true})
+    add_includedirs(sdkdir.."include/os",{public = true})
+    add_includedirs(sdkdir.."include/arch/xt804",{public = true})
+    add_includedirs(sdkdir.."include/arch/xt804/csi_core",{public = true})
 
--- target_end()
+    after_load(function (target)
+        for _, sourcebatch in pairs(target:sourcebatches()) do
+            if sourcebatch.sourcekind == "as" then -- only asm files
+                for idx, objectfile in ipairs(sourcebatch.objectfiles) do
+                    sourcebatch.objectfiles[idx] = objectfile:gsub("%.S%.o", ".o")
+                end
+            end
+        end
+    end)
+
+target_end()
 
 target("blehost")
     set_kind("static")
