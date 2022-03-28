3e03f663992a4953333779d66df2773fbb400f82
Dozingfiretruck <1473454180@qq.com>
2022-03-28 21:18:51 +0800

    fix:去掉无用代码

--- app/main.c
+++ app/main.c
@@ -104,26 +104,6 @@ extern unsigned int  TLS_FLASH_OTA_FLAG_ADDR        ;
 extern unsigned int  TLS_FLASH_END_ADDR             ;
 #endif
 
-#include "luat_rtos.h"
-#include "luat_server.h"
-// static tls_os_queue_t *shell_queue = NULL;
-// luat_rtos_queue_t queue = {
-// 	.userdata = shell_queue,
-// };
-// static luat_rtos_queue_t queue;
-
-// static void prvnes_Task(void* params){
-// 	int* tag;
-// 	while (1)
-// 	{
-// 		// ret = tls_os_queue_receive(shell_queue, (void **) &tag, 0, 0);
-// 		int ret = luat_queue_recv(&queue, tag, 0, 0);
-// 		printf("this is a test task!!!!\r\n");
-// 		tls_os_time_delay(1000);
-// 	}
-	
-// }
-
 void UserMain(void){
 	char unique_id [18] = {0};
 
@@ -138,17 +118,6 @@ void UserMain(void){
 	// 读取开机原因
 	rst_sta = tls_reg_read32(HR_CLK_RST_STA);
 	tls_reg_write32(HR_CLK_RST_STA, 0xFF);
-
-
-// luat_queue_create(&queue, 256, 0);
-
-// luat_thread_t nes_thread = {
-// 	.name = "nes",
-// 	.priority = 1,
-// 	.stack_size = 16*1024,
-// 	.entry = prvnes_Task,
-// };
-// luat_thread_start(&nes_thread);
 
 #ifdef LUAT_USE_SHELL
 	luat_shell_poweron(0);
@@ -230,14 +199,6 @@ TLS_FLASH_END_ADDR             =		  (0x80FFFFFUL);
 	tls_os_timer_create(&os_timer, lvgl_timer_cb, NULL, 10/(1000 / configTICK_RATE_HZ), 1, NULL);
 	tls_os_timer_start(os_timer);
 #endif
-// int a = 1;
-// while (a++)
-// {
-// 	// tls_os_queue_send(shell_queue, (void *)user_data, 0);
-// 	luat_queue_send(&queue, a,  0, 0);
-// 	tls_os_time_delay(1000);
-// }
-	luat_server_start();
 	tls_os_task_create(NULL, NULL,
 				luat_start,
 				NULL,
