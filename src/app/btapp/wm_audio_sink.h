1317c97e90f36cba25169d696e582590b638af07
Wendal Chen <wendal1985@gmail.com>
2022-08-01 19:56:23 +0800

    remove: 移除ble的非nimble实现

--- src/app/btapp/wm_audio_sink.h
+++
@@ -1,8 +0,0 @@
-#ifndef __WM_AUDIO_SINK_H__
-#define __WM_AUDIO_SINK_H__
-#include "wm_bt.h"
-extern tls_bt_status_t tls_bt_enable_a2dp_sink();
-extern tls_bt_status_t tls_bt_disable_a2dp_sink();
-
-
-#endif
--- src/app/btapp/wm_bt_spp_client.h
+++
@@ -1,9 +0,0 @@
-#ifndef __WM_BT_SPP_CLIENT_H__
-#define __WM_BT_SPP_CLIENT_H__
-
-tls_bt_status_t tls_bt_enable_spp_client(void);
-
-tls_bt_status_t tls_bt_disable_spp_client(void);
-
-#endif
-
