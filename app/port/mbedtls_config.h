633a54600ebd0255d26732c1ea5fcea45167c82a
Wendal Chen <wendal1985@gmail.com>
2022-09-26 11:19:16 +0800

    change: 启用MBEDTLS_DEPRECATED_REMOVED,禁止使用mbedtls的过时api

--- app/port/mbedtls_config.h
+++ app/port/mbedtls_config.h
@@ -217,7 +217,7 @@
  *
  * Uncomment to get errors on using deprecated functions.
  */
-//#define MBEDTLS_DEPRECATED_REMOVED
+#define MBEDTLS_DEPRECATED_REMOVED
 
 /* \} name SECTION: System support */
 
