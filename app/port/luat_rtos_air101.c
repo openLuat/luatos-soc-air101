bfb1165a50432f291d2ce0b51c1fe4d2ceecc6a0
Wendal Chen <wendal1985@gmail.com>
2022-11-02 22:36:34 +0800

    fix: 使得rtos_air101.c能编译通过,否则云编译跑不了

--- app/port/luat_rtos_air101.c
+++ app/port/luat_rtos_air101.c
@@ -33,55 +33,55 @@
 #define LUAT_LOG_TAG "luat.rtos"
 #include "luat_log.h"
 
-int luat_thread_start(luat_thread_t* thread){
-	thread->task_stk = luat_heap_malloc((thread->stack_size)*sizeof(uint32_t));
-	return tls_os_task_create(NULL, thread->name,(void (*)(void *param))thread->entry,NULL,thread->task_stk,(thread->stack_size)*sizeof(uint32_t),thread->priority,0);
-}
+// int luat_thread_start(luat_thread_t* thread){
+// 	thread->task_stk = luat_heap_malloc((thread->stack_size)*sizeof(uint32_t));
+// 	return tls_os_task_create(NULL, thread->name,(void (*)(void *param))thread->entry,NULL,thread->task_stk,(thread->stack_size)*sizeof(uint32_t),thread->priority,0);
+// }
 
-LUAT_RET luat_thread_stop(luat_thread_t* thread) {
-    return LUAT_ERR_FAIL;
-}
+// LUAT_RET luat_thread_stop(luat_thread_t* thread) {
+//     return LUAT_ERR_FAIL;
+// }
 
-LUAT_RET luat_thread_delete(luat_thread_t* thread) {
-	return LUAT_ERR_FAIL;
-}
+// LUAT_RET luat_thread_delete(luat_thread_t* thread) {
+// 	return LUAT_ERR_FAIL;
+// }
 
 
 
-void *luat_queue_create(size_t msgcount, size_t msgsize){
-	tls_os_queue_t *queue = (tls_os_queue_t *)luat_heap_malloc(sizeof(tls_os_queue_t));
-	tls_os_queue_create(&queue, msgcount);
-	return queue;
-}
+// void *luat_queue_create(size_t msgcount, size_t msgsize){
+// 	tls_os_queue_t *queue = (tls_os_queue_t *)luat_heap_malloc(sizeof(tls_os_queue_t));
+// 	tls_os_queue_create(&queue, msgcount);
+// 	return queue;
+// }
 
-LUAT_RET luat_queue_send(void*   queue, void* msg,  size_t msg_size, size_t timeout){
-	return tls_os_queue_send((tls_os_queue_t *)queue, msg, msg_size);
-}
+// LUAT_RET luat_queue_send(void*   queue, void* msg,  size_t msg_size, size_t timeout){
+// 	return tls_os_queue_send((tls_os_queue_t *)queue, msg, msg_size);
+// }
 
-LUAT_RET luat_queue_recv(void*   queue, void* msg, size_t msg_size, size_t timeout){
-	return tls_os_queue_receive((tls_os_queue_t *)queue, (void **) &msg, msg_size, timeout);
-}
+// LUAT_RET luat_queue_recv(void*   queue, void* msg, size_t msg_size, size_t timeout){
+// 	return tls_os_queue_receive((tls_os_queue_t *)queue, (void **) &msg, msg_size, timeout);
+// }
 
-LUAT_RET luat_queue_reset(void*   queue){
-	return tls_os_queue_flush((tls_os_queue_t *)queue);
-}
+// LUAT_RET luat_queue_reset(void*   queue){
+// 	return tls_os_queue_flush((tls_os_queue_t *)queue);
+// }
 
-LUAT_RET luat_queue_delete(void*   queue){
-	return tls_os_queue_delete((tls_os_queue_t *)queue);
-}
+// LUAT_RET luat_queue_delete(void*   queue){
+// 	return tls_os_queue_delete((tls_os_queue_t *)queue);
+// }
 
-LUAT_RET luat_queue_free(void*   queue){
-	luat_heap_free((tls_os_queue_t *)queue);
-	return 0;
-}
+// LUAT_RET luat_queue_free(void*   queue){
+// 	luat_heap_free((tls_os_queue_t *)queue);
+// 	return 0;
+// }
 
-LUAT_RET luat_send_event_to_task(void *task_handle, uint32_t id, uint32_t param1, uint32_t param2, uint32_t param3) {
-    return LUAT_ERR_OK;
-}
+// LUAT_RET luat_send_event_to_task(void *task_handle, uint32_t id, uint32_t param1, uint32_t param2, uint32_t param3) {
+//     return LUAT_ERR_OK;
+// }
 
-LUAT_RET luat_wait_event_from_task(void *task_handle, uint32_t wait_event_id, void *out_event, void *call_back, uint32_t ms) {
-    return LUAT_ERR_OK;
-}
+// LUAT_RET luat_wait_event_from_task(void *task_handle, uint32_t wait_event_id, void *out_event, void *call_back, uint32_t ms) {
+//     return LUAT_ERR_OK;
+// }
 
 // int luat_sem_create(luat_sem_t* semaphore){
 // 	semaphore->userdata = (tls_os_sem_t *)luat_heap_malloc(sizeof(tls_os_sem_t));
@@ -106,28 +106,28 @@ typedef struct luat_rtos_timer {
     tls_os_timer_t *timer;
 	void *cb;
 	void *param;
-}luat_rtos_timer_t;
+}air101_timer;
 
 void *luat_create_rtos_timer(void *cb, void *param, void *task_handle){
-	luat_rtos_timer_t *luat_timer = luat_heap_malloc(sizeof(luat_rtos_timer_t));
+	air101_timer *luat_timer = luat_heap_malloc(sizeof(air101_timer));
 	luat_timer->cb = cb;
 	luat_timer->param = param;
 	return luat_timer;
 }
 
 int luat_start_rtos_timer(void *timer, uint32_t ms, uint8_t is_repeat){
-	luat_rtos_timer_t *luat_timer = (luat_rtos_timer_t *)timer;
+	air101_timer *luat_timer = (air101_timer *)timer;
 	tls_os_timer_create(&(luat_timer->timer), luat_timer->cb, luat_timer->param,ms, is_repeat, NULL);
 	tls_os_timer_start(luat_timer->timer);
 	return 0;
 }
 
 void luat_stop_rtos_timer(void *timer){
-	luat_rtos_timer_t *luat_timer = (luat_rtos_timer_t *)timer;
+	air101_timer *luat_timer = (air101_timer *)timer;
 	tls_os_timer_stop(luat_timer->timer);
 }
 void luat_release_rtos_timer(void *timer){
-	luat_rtos_timer_t *luat_timer = (luat_rtos_timer_t *)timer;
+	air101_timer *luat_timer = (air101_timer *)timer;
 	tls_os_timer_delete(luat_timer->timer);
 	luat_heap_free(luat_timer);
 }
