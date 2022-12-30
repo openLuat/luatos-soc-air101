fe4c6dfb65337e644febb91f6fffa47b8b1f0a5a
Wendal Chen <wendal1985@gmail.com>
2022-12-30 23:43:59 +0800

    fix: 不小心把conf_bsp的其他修改也提交了

--- app/port/luat_conf_bsp.h
+++ app/port/luat_conf_bsp.h
@@ -5,7 +5,7 @@
 #define LUAT_BSP_VERSION "V0015"
 
 // Air101 与 Air103 的Flash大小有差异,需要区分
-#define AIR103
+#define AIR101
 
 // 启用64位虚拟机
 // #define LUAT_CONF_VM_64bit
@@ -40,12 +40,12 @@
 #define LUAT_USE_MCU  1
 #define LUAT_USE_RTC 1
 // SDIO 仅支持TF/SD卡的挂载
-// #define LUAT_USE_SDIO 1
+#define LUAT_USE_SDIO 1
 // 段码屏/段式屏, 按需启用
-// #define LUAT_USE_LCDSEG 1
+#define LUAT_USE_LCDSEG 1
 // OTP
-// #define LUAT_USE_OTP 1
-// #define LUAT_USE_TOUCHKEY 1
+#define LUAT_USE_OTP 1
+#define LUAT_USE_TOUCHKEY 1
 // i2s
 //#define LUAt_USE_I2S 1
 
@@ -55,7 +55,7 @@
 // #define LUAT_USE_DHCP 1
 // #define LUAT_USE_DNS 1
 
-// #define LUAT_USE_IOTAUTH 1
+#define LUAT_USE_IOTAUTH 1
 
 //----------------------------
 // 常用工具库, 按需启用, cjson和pack是强烈推荐启用的
@@ -63,27 +63,27 @@
 #define LUAT_USE_CJSON  1
 #define LUAT_USE_ZBUFF  1
 #define LUAT_USE_PACK  1
-// #define LUAT_USE_LIBGNSS  1
-// #define LUAT_USE_FS  1
-// #define LUAT_USE_SENSOR  1
-// #define LUAT_USE_SFUD  1
+#define LUAT_USE_LIBGNSS  1
+#define LUAT_USE_FS  1
+#define LUAT_USE_SENSOR  1
+#define LUAT_USE_SFUD  1
 // #define LUAT_USE_STATEM 1
 // 性能测试,跑完就是玩,不要与lvgl一起启用,生产环境的固件别加这个库
 // #define LUAT_USE_COREMARK 1
-// #define LUAT_USE_ZLIB 1 
-// #define LUAT_USE_IR 1
+#define LUAT_USE_ZLIB 1 
+#define LUAT_USE_IR 1
 // FDB 提供kv数据库, 与nvm库类似
 // #define LUAT_USE_FDB 1
 // fskv提供与fdb库兼容的API,旨在替换fdb库
 #define LUAT_USE_FSKV 1
-// #define LUAT_USE_OTA 1
-// #define LUAT_USE_I2CTOOLS 1
-// #define LUAT_USE_LORA 1
-// #define LUAT_USE_MLX90640 1
+#define LUAT_USE_OTA 1
+#define LUAT_USE_I2CTOOLS 1
+#define LUAT_USE_LORA 1
+#define LUAT_USE_MLX90640 1
 // zlib压缩,更快更小的实现
-// #define LUAT_USE_MINIZ 1
+#define LUAT_USE_MINIZ 1
 
-// #define LUAT_USE_RSA 1
+#define LUAT_USE_RSA 1
 
 // // 使用 TLSF 内存池, 实验性, 内存利用率更高一些
 // #define LUAT_USE_TLSF 1
@@ -103,35 +103,35 @@
 //----------------------------
 // 高级功能, 其中shell是推荐启用, 除非你打算uart0也读数据
 #define LUAT_USE_SHELL 1
-// #define LUAT_USE_DBG
+#define LUAT_USE_DBG
 // NIMBLE 是蓝牙功能, 名为BLE, 但绝非低功耗.
 // #define LUAT_USE_NIMBLE 1
 // 多虚拟机支持,实验性,一般不启用
 // #define LUAT_USE_VMX 1
-// #define LUAT_USE_PROTOBUF 1
+#define LUAT_USE_PROTOBUF 1
 
 //---------------------
 // UI
 // LCD  是彩屏, 若使用LVGL就必须启用LCD
-// #define LUAT_USE_LCD
-// #define LUAT_USE_TJPGD
+#define LUAT_USE_LCD
+#define LUAT_USE_TJPGD
 // EINK 是墨水屏
-// #define LUAT_USE_EINK
+#define LUAT_USE_EINK
 
 //---------------------
 // U8G2
 // 单色屏, 支持i2c/spi
-// #define LUAT_USE_DISP
-// #define LUAT_USE_U8G2
+#define LUAT_USE_DISP
+#define LUAT_USE_U8G2
 
 /**************FONT*****************/
-// #define LUAT_USE_FONTS
+#define LUAT_USE_FONTS
 /**********U8G2&LCD&EINK FONT*************/
-// #define USE_U8G2_OPPOSANSM_ENGLISH 1
+#define USE_U8G2_OPPOSANSM_ENGLISH 1
 // #define USE_U8G2_OPPOSANSM8_CHINESE
-// #define USE_U8G2_OPPOSANSM10_CHINESE
-// #define USE_U8G2_OPPOSANSM12_CHINESE
-// #define USE_U8G2_OPPOSANSM16_CHINESE
+#define USE_U8G2_OPPOSANSM10_CHINESE
+#define USE_U8G2_OPPOSANSM12_CHINESE
+#define USE_U8G2_OPPOSANSM16_CHINESE
 // #define USE_U8G2_OPPOSANSM24_CHINESE
 // #define USE_U8G2_OPPOSANSM32_CHINESE
 /**********LVGL FONT*************/
