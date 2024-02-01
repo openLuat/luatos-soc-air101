941849bb935e14ae97ee93045f7c81ff6b9a3431
Dozingfiretruck <1473454180@qq.com>
2024-02-01 14:06:55 +0800

    update:支持codec power pin

--- app/port/luat_audio_air101.c
+++ app/port/luat_audio_air101.c
@@ -9,7 +9,10 @@
 #define LUAT_LOG_TAG "audio"
 #include "luat_log.h"
 
-luat_audio_conf_t audio_hardware = {0};
+static luat_audio_conf_t audio_hardware = {
+    .codec_conf.power_pin = LUAT_CODEC_PA_NONE,
+    .codec_conf.pa_pin = LUAT_CODEC_PA_NONE,
+};
 
 luat_audio_conf_t *luat_audio_get_config(uint8_t multimedia_id){
     if (multimedia_id == 0) return &audio_hardware;
