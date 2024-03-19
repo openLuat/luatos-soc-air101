b033cb2a4dbd2681cede84179be670600c7f54c7
Wendal Chen <wendal1985@gmail.com>
2024-03-19 09:28:38 +0800

    fix: 恢复开启tls 1.0/1.1

--- app/port/mbedtls_config_air101.h
+++ app/port/mbedtls_config_air101.h
@@ -1263,7 +1263,7 @@
  *
  * Comment this macro to disable support for TLS 1.0
  */
-// #define MBEDTLS_SSL_PROTO_TLS1
+#define MBEDTLS_SSL_PROTO_TLS1
 
 /**
  * \def MBEDTLS_SSL_PROTO_TLS1_1
@@ -1275,7 +1275,7 @@
  *
  * Comment this macro to disable support for TLS 1.1 / DTLS 1.0
  */
-// #define MBEDTLS_SSL_PROTO_TLS1_1
+#define MBEDTLS_SSL_PROTO_TLS1_1
 
 /**
  * \def MBEDTLS_SSL_PROTO_TLS1_2
