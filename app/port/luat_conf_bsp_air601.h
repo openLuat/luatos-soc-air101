7073a06d20963f33b37356da122c408ed4322f74
Wendal Chen <wendal1985@gmail.com>
2026-03-17 12:35:56 +0800

    update: 版本号调整到V2001开始

--- app/port/luat_conf_bsp_air601.h
+++ app/port/luat_conf_bsp_air601.h
@@ -58,7 +58,7 @@
 #ifndef LUAT_CONF_BSP
 #define LUAT_CONF_BSP
 
-#define LUAT_BSP_VERSION "V1025"
+#define LUAT_BSP_VERSION "V1027"
 
 //------------------------------------------------------
 // 以下custom --> 到  <-- custom 之间的内容,是供用户配置的
